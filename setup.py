229bd2b8be52a38cdabc3985e4a302d7831e5426
Cyrus Rice <cyrusric@amazon.com>
2020-08-20 20:19:19 +0100

    n/a

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='techcookbook', 
+    version='0.1',
+    description='TechCookBook Python Packge', 
+    url='https://techcookbook.com', 
+    author='@yuki', 
+    author_email='', 
+    license='MIT', 
+    packages=['techcookbook'],
+    install_requires=
+    [
+        'PyMySQL'
+    ],      
+    zip_safe=False)
---
+++ techcookbook/__init__.py
@@ -0,0 +1,1 @@
+from caseconvert import *
